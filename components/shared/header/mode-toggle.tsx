fd8795b40808fcebaedb375036d9486d64c56f56
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-09-21 21:23:03 +0100

    Theme Mode Toggle

---
+++ components/shared/header/mode-toggle.tsx
@@ -0,0 +1,46 @@
+'use client'
+import {useTheme} from "next-themes";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuContent,
+    DropdownMenuCheckboxItem
+} from "@/components/ui/dropdown-menu";
+import {Button} from "@/components/ui/button";
+import {SunIcon,MoonIcon,SunMoon} from "lucide-react";
+
+const ModeToggle = () => {
+    const {theme, setTheme} = useTheme();
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant='ghost' className='focus-visible:ring-0 focus-visible:ring-offset-0'>
+                    {theme==='system'?(
+                        <SunMoon/>
+                    ):theme==='dark'?(
+                        <MoonIcon />
+                    ):(
+                        <SunIcon/>
+                    )}
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+                <DropdownMenuSeparator/>
+                <DropdownMenuCheckboxItem checked={theme==='system'} onClick={()=>setTheme('system')}>
+                    System
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuSeparator/>
+                <DropdownMenuCheckboxItem checked={theme==='dark'} onClick={()=>setTheme('system')}>
+                    Dark
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem checked={theme==='dark'} onClick={()=>setTheme('light')}>
+                    Light
+                </DropdownMenuCheckboxItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+export default ModeToggle
