b131eb470a58e934a9d6fdf75430a01e6fb5670e
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-09-22 19:54:07 +0100

    Sample Products & Products List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
 import {Button} from "@/components/ui/button";
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 const HomePage = () => {
     return (
-        <Button>Button</Button>
+        <>
+            <ProductList data={sampleData.products} title='Newest Arrivals' limit={4}/>
+        </>
     )
 }
 export default HomePage
