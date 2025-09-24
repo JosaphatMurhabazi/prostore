32fc3995c6fd95bdb8372b346a71e4d0004ccaa4
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-09-24 11:08:12 +0100

    Sign In & Sign Out Action

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,27 @@
+import {signInFormSchema} from "@/lib/validations";
+import {signIn,signOut} from "@/auth";
+import {isRedirectError} from "next/dist/client/components/redirect-error";
+
+// sign in the user with credentials
+export async function signInWithCredentials(prevState:unknown,formData:FormData){
+    try {
+        const user = signInFormSchema.parse({
+            email: formData.get('email'),
+            password: formData.get('password'),
+        })
+
+        await signIn('credentials',user)
+        return {success: true,message:'Sign In Successfully'}
+    }catch (error) {
+        if (isRedirectError(error)) {
+            throw error
+        }
+
+        return {success: false, message:'Invalid credentials'}
+    }
+}
+
+// Sign user out
+export async function signOutUser(){
+    await signOut();
+}
