87999ec370bb197defa399d2284494a17be39e8b
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-10-03 16:02:09 +0100

    Shipping Address Zod Schema & Page

--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,13 @@
 import {z} from 'zod';
-import {cartItemSchema, insertCartSchema, insertProductSchema} from "@/lib/validations";
+import {cartItemSchema, insertCartSchema, insertProductSchema,shippingAddressSchema} from "@/lib/validations";
 
-export type Product =z.infer<typeof insertProductSchema> & {
-    id:string;
-    rating:string;
-    createdAt:Date;
+export type Product = z.infer<typeof insertProductSchema> & {
+    id: string;
+    rating: string;
+    createdAt: Date;
 };
 
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
+
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
