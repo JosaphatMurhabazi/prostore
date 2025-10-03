fe19136ccd0feb7650c06ca893c92c7a5b599d9c
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-10-03 15:23:41 +0100

    Shadcn UI Table

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -1,19 +1,19 @@
 import {Metadata} from "next";
-import CartTable from './cart-table';
 import {getMyCart} from "@/lib/actions/cart.actions";
+import CartTable from "./cart-table";
 
-export const metadata:Metadata= {
-    title:'Shopping Cart'
+export const metadata: Metadata = {
+    title: 'Shopping Cart'
 }
 
 
-const CartPage = async() => {
+const CartPage = async () => {
     const cart = await getMyCart();
 
 
     return (
         <>
-            <CartTable/>
+            <CartTable cart={cart}/>
         </>
     )
 }
