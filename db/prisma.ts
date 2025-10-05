7f44ac33eb5ec368ff2b905bd07bc11d396e137c
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-10-05 22:24:00 +0100

    Format Utility functions

--- db/prisma.ts
+++ db/prisma.ts
@@ -7,28 +7,87 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 neonConfig.webSocketConstructor = ws;
 const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaNeon({ connectionString });
+const adapter = new PrismaNeon({connectionString});
 
 export const prisma =
-    globalForPrisma.prisma || new PrismaClient({ adapter }).$extends({
-      result: {
-        product: {
-          price: {
-            needs: { price: true },
-            compute(product: Product) {
-              return product.price.toString();
+    globalForPrisma.prisma || new PrismaClient({adapter}).$extends({
+        result: {
+            product: {
+                price: {
+                    needs: {price: true},
+                    compute(product: Product) {
+                        return product.price.toString();
+                    },
+                },
+                rating: {
+                    needs: {rating: true},
+                    compute(product: Product) {
+                        return product.rating.toString();
+                    },
+                },
             },
-          },
-          rating: {
-            needs: { rating: true },
-            compute(product:Product) {
-              return product.rating.toString();
+            cart: {
+                itemsPrice: {
+                    needs: {itemsPrice: true},
+                    compute(cart) {
+                        return cart.itemsPrice.toString();
+                    }
+                },
+                shippingPrice: {
+                    needs: {shippingPrice: true},
+                    compute(cart) {
+                        return cart.shippingPrice.toString();
+                    }
+                },
+                taxPrice: {
+                    needs: {taxPrice: true},
+                    compute(cart) {
+                        return cart.taxPrice.toString();
+                    }
+                },
+                totalPrice: {
+                    needs: {totalPrice: true},
+                    compute(cart) {
+                        return cart.totalPrice.toString();
+                    }
+                }
             },
-          },
+            order: {
+                itemsPrice: {
+                    needs: {itemsPrice: true},
+                    compute(cart) {
+                        return cart.itemsPrice.toString();
+                    }
+                },
+                shippingPrice: {
+                    needs: {shippingPrice: true},
+                    compute(cart) {
+                        return cart.shippingPrice.toString();
+                    }
+                },
+                taxPrice: {
+                    needs: {taxPrice: true},
+                    compute(cart) {
+                        return cart.taxPrice.toString();
+                    }
+                },
+                totalPrice: {
+                    needs: {totalPrice: true},
+                    compute(cart) {
+                        return cart.totalPrice.toString();
+                    }
+                }
+            },
+            orderItem:{
+                price:{
+                    compute(cart){
+                        return cart.price.toString();
+                    }
+                }
+            }
         },
-      },
     });
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+    globalForPrisma.prisma = prisma;
 }
